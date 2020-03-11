1aa86d7744b35c7e08d6fd8c45db72e3854993bf
dimiggy <dim_inglezos@hotmail.com>
2020-03-11 14:57:38 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get you, edit <code>src/App.js</code> and save to reload.
+          To aekara, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
